90bca4876121d6f8e34624e52d500863ffc0e589
mario-mra <mario.hi52@gmail.com>
2018-09-25 16:16:31 +0200

    Text to SVG file

--- python/TextGen.py
+++ python/TextGen.py
@@ -24,4 +24,4 @@ def make_svg_texts(text, sep=' ', out_dir='./text_out', rand_rotate=[0, 0], font
 
 
 if __name__ == '__main__':
-    make_svg_texts("#PapaPitufo #Amadeame #Maravilla #ITI #ITIANOS #Python #C #C++ #Java #html #php #MeExplicó #Aranzazu #Sampersan #Moya #Polaski #PDF'S #Tirado #Cruasán #Franco #Nazi #BlackAcademy #Paquito #PayoJr #Chuwi #Chatarramovil #Copito #Gilperez #Rosendo #RasberryPi #Pisapapeles #Cubatas #Potenciómetro #Aguilucho #MeCageEnDios #TFG #Novelista #9,9 #Erasmus2018 #Jubilados #Gálvez #Surfero #SeñoritaReyes #Luisiño #QueTePeines #VivaFranco #CadaPerroSeLameSuCipote #ArrozGuineano #Nyquist #Coriolis #HastaLuegoMariCarmen #NedFlanders #Cabañas #Novato #Becario #Zenio #Matlab #Arduino")
+    make_svg_texts("Word1 Word2 Word3")
